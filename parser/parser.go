c95e46c6e1ac1f66b5b8c673cdf2972ff41610c8
Gabriel Barros <gabriel.sb199@gmail.com>
2025-04-26 19:29:11 -0300

    feat: add service

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,11 @@
 package parser
+
+import (
+	"net/http"
+)
+
+type Parser struct{}
+
+func (p *Parser) Parse() (*http.Request, error) {
+	return &http.Request{}, nil
+}
--- store/store.go
+++ store/store.go
@@ -1,3 +1,40 @@
 package store
 
-type Store struct{}
+import (
+	"errors"
+	"net/http"
+
+	"github.com/felipefbs/lazy-request/parser"
+)
+
+type Store struct {
+	parser parser.Parser
+}
+
+func New(p parser.Parser) *Store {
+	return &Store{p}
+}
+
+func (s *Store) GetRequest() (*http.Request, error) {
+	request, err := s.parser.Parse()
+	if err != nil {
+		return &http.Request{}, errors.New("error reading requisition")
+	}
+	return request, nil
+}
+
+func (s *Store) ExecuteRequest() (http.Response, error) {
+	request, err := s.GetRequest()
+	if err != nil {
+		return http.Response{}, err
+	}
+
+	c := http.DefaultClient
+
+	res, err := c.Do(request)
+	if err != nil {
+		return http.Response{}, err
+	}
+
+	return *res, nil
+}
